e42fc67ce91fcecc7603220b3e46833282a7053c
msvaldez <msvaldez@d9f667b1-f3fe-58c0-f3a1-00d64fa5a428>
2012-04-03 22:31:06 +0000

    Added sdgran() (function used by qg5()) and made BlzSim a non-static class. Also added BlzSimCommon struct to hold all the Fortran variables that are part of COMMON blocks

--- blzsim.h
+++ blzsim.h
@@ -7,24 +7,41 @@ using namespace std;
 static const int BLZSIM_DIM16384 = 16384;
 static const int BLZSIM_DIM32768 = 32768;
 
+// These are variables in the TEMZ (Fortran) "common" blocks. Putting
+// them here in a structure for now, to expedited porting to C++.
+// Can come up with a better way later
+typedef struct {
+  double bdx, bdy, bdz, gammad, betad; // cvel
+  double zred1, bfld, bperpp; // cparm
+  double dcsth1, dcsth2, dsnth1, dsnth2, dsang, tdust; // cdust
+  double snu[68], ssseed[68], nuhi; // cssc
+  double ggam[44], edist[44]; // cdist
+  double dustnu[22],dusti[22]; // cseed
+  double freq; // cfreq
+} BlzSimCommon;
+
+double sdgran(double sn, void *pObject);
+
 /**
-   Various simulation routines
+   Top-level simulation object and supporting methods
  */
 class BlzSim {
 
  public:
+  BlzSim();
+  ~BlzSim();
+
+  BlzSimCommon common;
+
   // code is ported from Fortran psdsim() in temz.f from Ritaban Chatterjee
   // to create variations according to an input PSD with slopes beta1 and beta2 at
   // variational frequencies below and above a break frequency nu_break */
   // N=number of data points in the lc, should be an integer power of 2, N<=8192
   // t_incre1=increment in time in each step while resampling the simulated data.
   // This must be larger than the smallest interval between successive data points in the input light curve
-  static void psdsim(const int N, const float beta1, const float beta2, const float nu_break, const float t_incre1, 
-                     float *lc_sim);
+  void psdsim(const int N, const float beta1, const float beta2, const float nu_break, const float t_incre1, 
+              float *lc_sim);
 
- private:
-  BlzSim();
-  ~BlzSim();
 };
     
 #endif // _INCL_BLZSIM_H_

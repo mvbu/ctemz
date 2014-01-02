7fd2044fa8b895dcbb154d252dd6ead5182fea77
msvaldez <msvaldez@d9f667b1-f3fe-58c0-f3a1-00d64fa5a428>
2014-01-02 19:47:12 +0000

    Added class to keep track of indices used. Initially needed for distributing frequencies among threads running ssc(freq)

--- blzutil.cpp
+++ blzutil.cpp
@@ -25,3 +25,52 @@ int BlzUtil::getNumProcessors()
 
   return retVal;
 }
+
+BlzIndexTracker::BlzIndexTracker(int startIndex, int endIndex):
+  iStart(startIndex),iEnd(endIndex),range(endIndex-startIndex+1)
+{
+  // allocate enough space for the indices
+  pIndices = (bool*)malloc(range*sizeof(bool));
+  init();
+}
+
+BlzIndexTracker::~BlzIndexTracker() 
+{
+  if(pIndices != NULL) 
+    free(pIndices);
+}
+
+
+void BlzIndexTracker::init()
+{
+  int i;
+  for(i=0; i<range; i++) {
+    pIndices[i] = false;
+  }
+}
+
+void BlzIndexTracker::reset() { init(); }
+
+int BlzIndexTracker::getNextIndex(int currentIndex)
+{
+  if((currentIndex >= iStart) && (currentIndex < iEnd)) {
+    // If a valid index was passed, try to return the next consecutive index.
+    // if it is "taken", then just return the lowest index that is not already taken.
+    if(!pIndices[currentIndex-iStart+1]) {
+      pIndices[currentIndex-iStart+1] = true;
+      return currentIndex+1;
+    }
+  }
+
+  // Just try to return the lowest index that is not already taken
+  int i;
+  for(i=iStart; i<=iEnd; i++) {
+    if(!pIndices[i-iStart]) {
+      pIndices[i-iStart] = true;
+      return i;
+    }
+  }
+
+  // If all indices are taken, return -1
+  return -1;
+}
--- blzutil.h
+++ blzutil.h
@@ -4,11 +4,37 @@
 class BlzUtil {
  public:
   static int getNumProcessors();
+  template<class T> 
+    static bool arraysEqual(T array1[], T array2[], int nSize) {
+    int i;
+    for(i=0; i< nSize; i++) {
+      if(array1[i] != array2[i])
+        return false;
+    }
+    return true;
+  }
 
  private:
   BlzUtil();
   ~BlzUtil();
 };
-    
+
+// Class to assign indices to the caller and keep track of which indices are taken
+class BlzIndexTracker {
+ public:
+  BlzIndexTracker(int startIndex, int endIndex);
+  ~BlzIndexTracker();
+  void reset();
+
+  // Get next unused index. Pass in current index in case it's useful.
+  // Probably want to try and give the next consecutive index if it's available
+  int getNextIndex(int currentIndex=-1);
+
+ private:
+  void init();
+  bool *pIndices;
+  int range, iStart, iEnd;
+};
+
 #endif // _INCL_BLZUTIL_H_
 
